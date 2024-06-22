746ad4ad53c5e5da7479378a209f3b03e27c78f5
mthsrnn <matheusfreitas.pl070@academico.ifsul.edu.br>
2024-06-22 20:58:39 -0300

    updated plugins

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,40 +1,41 @@
 return {
 
     {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
 
     {
-  "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
-  lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
-  dependencies = {
-    {'williamboman/mason-lspconfig.nvim', lazy=false, opts = {ensure_installed = {"lua_ls", "clangd"}}},
-    -- main one
-    { "ms-jpq/coq_nvim", branch = "coq" },
+        "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+        lazy = false,            -- REQUIRED: tell lazy.nvim to start this plugin at startup
+        dependencies = {
+            { 'williamboman/mason-lspconfig.nvim', lazy = false,        opts = { ensure_installed = { "lua_ls", "clangd", "tsserver" } } },
+            -- main one
+            { "ms-jpq/coq_nvim",                   branch = "coq" },
 
-    -- 9000+ Snippets
-    { "ms-jpq/coq.artifacts", branch = "artifacts" },
+            -- 9000+ Snippets
+            { "ms-jpq/coq.artifacts",              branch = "artifacts" },
 
-    { 'ms-jpq/coq.thirdparty', branch = "3p" },
-  },
-  init = function()
-    vim.g.coq_settings = {
-        auto_start = 'shut-up', -- if you want to start COQ at startup
-    }
-  end,
-  config = function()
-    -- Your LSP settings here
-local lsp = require "lspconfig"
-local coq = require "coq" -- add this
+            { 'ms-jpq/coq.thirdparty',             branch = "3p" },
+        },
+        init = function()
+            vim.g.coq_settings = {
+                auto_start = 'shut-up', -- if you want to start COQ at startup
+            }
+        end,
+        config = function()
+            -- Your LSP settings here
+            local lsp = require "lspconfig"
+            local coq = require "coq" -- add this
 
-      lsp.tsserver.setup(coq.lsp_ensure_capabilities({capabilities = capabilities }))
-      lsp.html.setup(coq.lsp_ensure_capabilities({capabilities = capabilities }))
-     lsp.lua_ls.setup(coq.lsp_ensure_capabilities({capabilities = capabilities }))
-     lsp.clangd.setup(coq.lsp_ensure_capabilities({capabilities = capabilities }))
-    end,
+            lsp.tsserver.setup(coq.lsp_ensure_capabilities({}))
+            lsp.html.setup(coq.lsp_ensure_capabilities({}))
+            lsp.lua_ls.setup(coq.lsp_ensure_capabilities({}))
+            lsp.clangd.setup(coq.lsp_ensure_capabilities({}))
+            lsp.tsserver.setup(coq.lsp_ensure_capabilities({}))
+        end,
 
-},}
+    }, }
